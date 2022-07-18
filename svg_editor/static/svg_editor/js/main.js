883c993600b3128ca5e3a5bc150a3bac925a1ae5
wizardOfOz <gasaevgamid@gmail.com>
2022-07-18 12:12:21 +0300

    Добавлены новые функции
    - Возможность переключения слоя
    - Возможность отключения слоя

--- svg_editor/static/svg_editor/js/main.js
+++ svg_editor/static/svg_editor/js/main.js
@@ -7,46 +7,64 @@ const layerControlPanel = document.querySelector('#layer-control-panel');
 const layerNote = function() {
     let note = document.createElement('div');
     note.setAttribute('class','layer_note');
+    note.setAttribute('onclick','selectLayer(this)');
     note.insertAdjacentHTML('beforeend',`
-    <button onclick="enableVisibility(this)"></button>
-    <label style="margin: auto"></label>`);
+    <input onclick="checkVisibility(this,event)" type="checkbox" checked/>
+    <label style="margin: auto; -webkit-user-select: none"></label>`);
     return note;
 }();
 
-let currentLayer = {
-    layer: null,
-    note: null,
-};
-
+let currentLayer = null;
 let i = 0;
 
 function createLayer(){
     let draw = SVG().addTo(workspace).size(workspace.clientWidth, workspace.clientHeight);
-    draw.node.setAttribute('class', 'layer');
-    currentLayer.layer = draw.node;
+    let newLayer = draw.node;
+    newLayer.setAttribute('class', 'layer');
     
     opacitySlider.value = 1;
     
-    let newNote = layerNote.cloneNode(true); 
+    let newNote = layerNote.cloneNode(true);
+    newNote.setAttribute('checked',false);
     newNote.lastElementChild.textContent = 'Layer '+i++;
-    layerControlPanel.append(newNote);
-    currentLayer.note = newNote;
+    layerControlPanel.prepend(newNote);
+
+    newLayer.note = newNote;
+    newNote.layer = newLayer;
+    selectLayer(newNote);
 }
 
 function changeOpacity() {
-    currentLayer.layer.setAttribute('opacity', opacitySlider.value);
+    currentLayer.setAttribute('opacity', opacitySlider.value);
+}
+
+function checkVisibility(checkbox,ev) {
+    ev.stopPropagation();
+    let layer = checkbox.parentElement.layer;
+    if (checkbox.checked) {
+        layer.setAttribute('display', '');
+        return;
+    }
+    layer.setAttribute('display', 'none');
+}
+
+function selectLayer(note) {
+    if (currentLayer !== null) {
+        currentLayer.note.setAttribute('checked','');
+    }
+    note.setAttribute('checked',note.getAttribute('checked') === 'true' ? false : true);
+    currentLayer = note.layer;
 }
 
 function deleteLayer() {
-    if (currentLayer.layer == null) return;
-    currentLayer.layer.remove();
-    currentLayer.layer = null;
+    if (currentLayer === null) return;
+    currentLayer.remove();
     currentLayer.note.remove();
-    currentLayer.note = null;
+    currentLayer = null;
 }
 
 function drawPlease() {
-    if (currentLayer.layer == null) return;
+    if (currentLayer === null) return;
     var a_x, a_y, b_x, b_y, c_x, c_y;
     a_x = document.getElementById('coord_a_x').value;
     a_y = document.getElementById('coord_a_y').value;
@@ -54,12 +72,12 @@ function drawPlease() {
     b_y = document.getElementById('coord_b_y').value;
     c_x = document.getElementById('coord_c_x').value;
     c_y = document.getElementById('coord_c_y').value;
-    SVG(currentLayer.layer).polygon([[a_x, a_y], [b_x, b_y], [c_x, c_y]]).fill(document.getElementById("color").value).stroke({ width: 1 })
+    SVG(currentLayer).polygon([[a_x, a_y], [b_x, b_y], [c_x, c_y]]).fill(document.getElementById("color").value).stroke({ width: 1 })
 }
 
 function clearPlease() {
-    if (currentLayer.layer == null) return;
-    SVG(currentLayer.layer).clear();
+    if (currentLayer === null) return;
+    SVG(currentLayer).clear();
 }
 
 $(document).ready(function () {
