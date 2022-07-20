0342691e0a9d776d8f36d339ab74bb0c76c2ef82
vyydra <67114367+vyydra@users.noreply.github.com>
2022-07-20 23:08:39 +0300

    Проверка работы функций работы с файлами

--- svg_editor/static/svg_editor/js/main.js
+++ svg_editor/static/svg_editor/js/main.js
@@ -1,103 +1,201 @@
-'use strict'
+var num_layers = 0
 
-const opacitySlider = document.querySelector('#opacity-slider');
-const workspace = document.querySelector('#workspace');
-const layerControlPanel = document.querySelector('#layer-control-panel');
-
-const layerNote = function() {
-    let note = document.createElement('div');
-    note.setAttribute('class','layer_note');
-    note.setAttribute('onclick','selectLayer(this)');
-    note.insertAdjacentHTML('beforeend',`
-    <input onclick="checkVisibility(this,event)" type="checkbox" checked/>
-    <label style="margin: auto; -webkit-user-select: none"></label>`);
-    return note;
-}();
-
-let currentLayer = null;
-let i = 0;
-
-function createLayer(){
-    let draw = SVG().addTo(workspace).size(workspace.clientWidth, workspace.clientHeight);
-    let newLayer = draw.node;
-    newLayer.setAttribute('class', 'layer');
-    
-    opacitySlider.value = 1;
-    
-    let newNote = layerNote.cloneNode(true);
-    newNote.setAttribute('checked',false);
-    newNote.lastElementChild.textContent = 'Layer '+i++;
-    layerControlPanel.prepend(newNote);
-
-    newLayer.note = newNote;
-    newNote.layer = newLayer;
-    selectLayer(newNote);
+function clearInputForm() {
+    $("input[name='file_name']").val("untitled");
+    $("input[name='width']").val("0");
+    $("input[name='height']").val("0");
 }
 
-function changeOpacity() {
-    currentLayer.setAttribute('opacity', opacitySlider.value);
-}
 
-function checkVisibility(checkbox,ev) {
-    ev.stopPropagation();
-    let layer = checkbox.parentElement.layer;
-    if (checkbox.checked) {
-        layer.setAttribute('display', '');
-        return;
-    }
-    layer.setAttribute('display', 'none');
-}
-
-function selectLayer(note) {
-    if (currentLayer !== null) {
-        currentLayer.note.setAttribute('checked','');
-    }
-    note.setAttribute('checked',note.getAttribute('checked') === 'true' ? false : true);
-    currentLayer = note.layer;
-}
-
-function deleteLayer() {
-    if (currentLayer === null) return;
-    currentLayer.remove();
-    currentLayer.note.remove();
-    currentLayer = null;
-}
-
-function drawPlease() {
-    if (currentLayer === null) return;
-    var a_x, a_y, b_x, b_y, c_x, c_y;
-    a_x = document.getElementById('coord_a_x').value;
-    a_y = document.getElementById('coord_a_y').value;
-    b_x = document.getElementById('coord_b_x').value;
-    b_y = document.getElementById('coord_b_y').value;
-    c_x = document.getElementById('coord_c_x').value;
-    c_y = document.getElementById('coord_c_y').value;
-    SVG(currentLayer).polygon([[a_x, a_y], [b_x, b_y], [c_x, c_y]]).fill(document.getElementById("color").value).stroke({ width: 1 })
-}
-
-function clearPlease() {
-    if (currentLayer === null) return;
-    SVG(currentLayer).clear();
-}
-
-/*$(document).ready(function () {
-    $('#saveButton').click(function () {
-        $.ajax({
-            data: {
-                svg: draw.svg(),
-                file_name: document.getElementById('file_name').value,
-                csrfmiddlewaretoken: '{{ csrf_token}}'
-            },
-            type: 'POST',
-            url: "{% url 'files_save' %}",
-            success: function (response) {
-                alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно создан!');
-            },
-            error: function (response) {
-                alert(response.responseJSON.errors);
-                console.log(response.responseJSON.errors)
-            }
-        });
-        return false;
+$(document).ready(function () {
+    /* Выпадающие кнопки сверху */
+    $("button.drop-button").on("click", function () {
+        $(".drop-content").css("display", "none");
+        if ($(this).data("clicked")) {
+            $(this).data("clicked", false)
+            $(this).removeClass("selected");
+            $(".drop-content").css("display", "none");
+        } else {
+            $("button.drop-button").removeClass("selected");
+            $("button.drop-button").data("clicked", false)
+            $(this).data("clicked", true)
+            $(this).addClass("selected");
+            $(this).parent().find(".drop-content").css("display", "block");
+        }
     });
-})*/
+
+    /* Закрытие из любого места */
+    $(document).click(function () {
+        $("button.drop-button").removeClass("selected");
+        $("button.drop-button").data("clicked", false)
+        $(".drop-content").css("display", "none");
+    });
+
+    $("button.drop-button").click(function(e) {
+        e.stopPropagation();
+    });
+
+    /* Обработка меню нового файла */
+    $(".drop-moving-button[name='new-file']").on("click", function () {
+        $("#new-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $(".ok-button").on("click", function () {
+        $(this).parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+        clearInputForm();
+        /*  if $(this).parent().attr('id') == ... -> создай новый файл/загрузи и тд
+            или для каждого случая делать отдельную кнопку
+        */
+
+    });
+
+    $(".drop-moving-button[name='save-file']").on("click", function() {
+        $("#save-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#save-button").on("click", function () {
+        $(this).parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+        clearInputForm();
+    });
+
+    $(".drop-moving-button[name='download-file']").on("click", function() {
+        $("#download-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#download-button").on("click", function () {
+        $(this).parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+        clearInputForm();
+    });
+
+    $(".drop-moving-button[name='open-file']").on("click", function() {
+        $("#open-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#file").on("input", function () {
+        $(this).parent().parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+        clearInputForm();
+    });
+
+    $(".close-menu-button").on("click", function () {
+        $(this).parent().parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+        clearInputForm();
+    });
+
+    /* Работа со слоями */
+    $("#layers-panel").on("click", ".layer-button", function () {
+        if ($(this).data("clicked")) {
+            $(this).data("clicked", false)
+            $(this).removeClass("selected");
+        } else {
+            $("button.layer-button").removeClass("selected");
+            $("button.layer-button").data("clicked", false);
+            $(this).data("clicked", true)
+            $(this).addClass("selected")
+        }
+    });
+
+    /* Нужно поправить добавление слоев (иногда отрицательные или с одинаковой цифрой) */
+    $("button#layers-panel-control-button.new").on("click", function () {
+        num_layers++;
+        var appended_layer = '<div class="layer"><button class="layer-button">Layer ' + num_layers + '</button></div>';
+        $(this).parents("#layers-panel").append(appended_layer);
+    });
+    $("button#layers-panel-control-button.delete").on("click", function () {
+        if ($(".layer-button.selected").length) {
+            num_layers--;
+            $(".layer-button.selected").parent().remove();
+            $(".layer-button.selected").remove();
+        }
+    });
+
+    $.ajaxSetup({
+        headers: { "X-CSRFToken": '{{csrf_token}}' }
+      });
+      $('#save-button').click(function () {
+          let svg_data = "<svg xmlns=\"http://www.w3.org/2000/svg\"" +
+          " version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" " +
+          "xmlns:svgjs=\"http://svgjs.com/svgjs\" width=\"" + workspace.clientWidth + "\" height=\"" + workspace.clientHeight + "\" class=\"layer\">"
+          for (i = 0; i < document.getElementsByClassName('layer').length; i++){
+          svg_data += '<g>'+ document.getElementsByClassName('layer')[i].innerHTML+'</g>'
+          }
+          svg_data += '</svg>'
+    console.log(svg_data)
+          $.ajax({
+              data: {
+                  svg: svg_data,
+                  file_name: document.getElementById('file_name').value,
+              },
+              type: 'POST',
+              url: "{% url 'files_save' %}",
+              success: function (response) {
+                  alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно создан!');
+              },
+              error: function (response) {
+                  alert(response.responseJSON.errors);
+                  console.log(response.responseJSON.errors);
+              }
+          });
+          return false;
+      });
+      $('#file').change(function () {
+          let data = new FormData();
+          data.append('file', $("#file")[0].files[0]);
+          $.ajax({
+              data: data,
+              type: 'POST',
+              url: "{% url 'files_upload' %}",
+              processData: false,
+              cache: false,
+              contentType: false,
+              success: function (response) {
+                  alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно загружен!');
+              },
+              error: function (response) {
+                  alert(response.responseJSON.errors);
+                  console.log(response.responseJSON.errors);
+              }
+          });
+          return false;
+      });
+      $('#download-button').click(function () {
+          $.ajax({
+               url: "{% url 'files_download' %}",
+               type: 'GET',
+              data: {
+                   file_name: document.getElementById('download_file_name').value
+              },
+               success: function(response) {
+                   let a = document.createElement("a");
+                   a.href = "/files_download?file_name=" + document.getElementById('download_file_name').value;
+                  a.click();
+               },
+               error: function(response){
+                   alert(response.responseJSON.errors);
+                  console.log(response.responseJSON.errors);
+               },
+           })
+      });
+})
