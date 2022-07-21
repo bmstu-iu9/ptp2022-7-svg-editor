933d73cc9282478c57ef355bb0a688fb531f71b1
Kabane-UN <andryushka.kaban.z@gmail.com>
2022-07-22 00:48:43 +0300

    Добавлена проверка уникальности почты

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 # Custom signup form
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Обязательное поле. Введите свою почту.')
+    email = forms.EmailField(max_length=200, help_text='Required field. Enter your email')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email exists")
+        return email
--- account/views.py
+++ account/views.py
@@ -2,8 +2,7 @@ import os
 from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
