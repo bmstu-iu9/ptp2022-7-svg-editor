f7d5b03d5e32fda61cc828986f86756dbb42a652
vyydra <67114367+vyydra@users.noreply.github.com>
2022-07-22 19:08:44 +0300

    Добавлены функции меню и рисования треугольника

--- svg_editor/static/svg_editor/js/main.js
+++ svg_editor/static/svg_editor/js/main.js
@@ -1,7 +1,7 @@
 var num_layers = 0
 
 function clearInputForm() {
-    $("input[name='filename']").val("untitled");
+    $("input[name='file_name']").val("untitled");
     $("input[name='width']").val("0");
     $("input[name='height']").val("0");
 }
@@ -23,18 +23,25 @@ $(document).ready(function () {
             $(this).parent().find(".drop-content").css("display", "block");
         }
     });
+
     /* Закрытие из любого места */
     $(document).click(function () {
         $("button.drop-button").removeClass("selected");
         $("button.drop-button").data("clicked", false)
         $(".drop-content").css("display", "none");
     });
+
     $("button.drop-button").click(function(e) {
         e.stopPropagation();
     });
+
     /* Обработка меню нового файла */
     $(".drop-moving-button[name='new-file']").on("click", function () {
         $(".file-menu-panel").css({
+            'display': 'none',
+            'z-index': '0'
+        })
+        $("#new-menu").css({
             'display': 'block',
             'z-index': '1'
         });
@@ -45,11 +52,59 @@ $(document).ready(function () {
             'z-index': '0'
         });
         clearInputForm();
-        /*  if $(this).parent().attr('id') == ... -> создай новый файл/загрузи и тд
-            или для каждого случая делать отдельную кнопку
-        */
-
     });
+
+    $(".drop-moving-button[name='save-file']").on("click", function() {
+        $(".file-menu-panel").css({
+            'display': 'none',
+            'z-index': '0'
+        })
+        $("#save-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#save-button").on("click", function () {
+        $(this).parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+    });
+
+    $(".drop-moving-button[name='download-file']").on("click", function() {
+        $(".file-menu-panel").css({
+            'display': 'none',
+            'z-index': '0'
+        })
+        $("#download-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#download-button").on("click", function () {
+        $(this).parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+    });
+
+    $(".drop-moving-button[name='open-file']").on("click", function() {
+        $(".file-menu-panel").css({
+            'display': 'none',
+            'z-index': '0'
+        })
+        $("#open-menu").css({
+            'display': 'block',
+            'z-index': '1'
+        });
+    });
+    $("#file").on("input", function () {
+        $(this).parent().parent().css({
+            'display': 'none',
+            'z-index': '0'
+        });
+    });
+
     $(".close-menu-button").on("click", function () {
         $(this).parent().parent().css({
             'display': 'none',
@@ -57,6 +112,7 @@ $(document).ready(function () {
         });
         clearInputForm();
     });
+
     /* Работа со слоями */
     $("#layers-panel").on("click", ".layer-button", function () {
         if ($(this).data("clicked")) {
@@ -69,6 +125,7 @@ $(document).ready(function () {
             $(this).addClass("selected")
         }
     });
+
     /* Нужно поправить добавление слоев (иногда отрицательные или с одинаковой цифрой) */
     $("button#layers-panel-control-button.new").on("click", function () {
         num_layers++;
@@ -82,4 +139,22 @@ $(document).ready(function () {
             $(".layer-button.selected").remove();
         }
     });
-});
+})
+
+const canvas = document.querySelector('#canvas');
+var draw = SVG(canvas).size(500, 500);
+		
+function drawPlease() {
+	var a_x, a_y, b_x, b_y, c_x, c_y;
+	a_x = document.getElementById('coord_a_x').value;
+	a_y = document.getElementById('coord_a_y').value;
+	b_x = document.getElementById('coord_b_x').value;
+	b_y = document.getElementById('coord_b_y').value;
+	c_x = document.getElementById('coord_c_x').value;
+	c_y = document.getElementById('coord_c_y').value;
+	draw.polygon([[a_x, a_y], [b_x, b_y], [c_x, c_y]]).fill(document.getElementById("color").value).stroke({ width: 1 })
+}
+		
+function clearPlease() {
+	draw.clear();
+}
