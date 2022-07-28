5230b3a42193e2032616a520ccb413f87b0b3318
Kabane-UN <andryushka.kaban.z@gmail.com>
2022-07-28 17:20:28 +0300

    Исправлены баги и произведен рефакторинг
    - Улучшены отступы в обоих main.js
    - Написаны комментарии в обоих main.js

--- account/static/account/js/main.js
+++ account/static/account/js/main.js
@@ -4,69 +4,68 @@
 
 $(document).ready(function () {
 
-	$.ajaxSetup({
-		headers: { "X-CSRFToken": token }
-	});
+    // Get csrf_token
+    $.ajaxSetup({
+        headers: {"X-CSRFToken": token}
+    });
 
-	$('#id_username').keyup(function () {
-		$.ajax({
-			data: $(this).serialize(),
-			type: 'GET',
-			url: usernameValidationURL,
-			success: function (response) {
-				if (response.exists) {
-					$('#id_username').removeClass('is-valid').addClass('is-invalid');
-				} else {
-					$('#id_username').removeClass('is-invalid').addClass('is-valid');
-				}
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Send username to server to validate it
+    $('#id_username').keyup(function () {
+        $.ajax({
+            data: $(this).serialize(),
+            type: 'GET',
+            url: usernameValidationURL,
+            success: function (response) {
+                if (response.exists) {
+                    $('#id_username').removeClass('is-valid').addClass('is-invalid');
+                } else {
+                    $('#id_username').removeClass('is-invalid').addClass('is-valid');
+                }
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$('#id_email').keyup(function () {
-		$.ajax({
-			data: $(this).serialize(),
-			type: 'GET',
-			url: emailValidationURL,
-			success: function (response) {
-				if (response.exists) {
-					$('#id_email').removeClass('is-valid').addClass('is-invalid');
-				} else {
-					$('#id_email').removeClass('is-invalid').addClass('is-valid');
-				}
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Send email to server to validate it
+    $('#id_email').keyup(function () {
+        $.ajax({
+            data: $(this).serialize(),
+            type: 'GET',
+            url: emailValidationURL,
+            success: function (response) {
+                if (response.exists) {
+                    $('#id_email').removeClass('is-valid').addClass('is-invalid');
+                } else {
+                    $('#id_email').removeClass('is-invalid').addClass('is-valid');
+                }
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$('#id_password2, #id_password1').keyup(function (){
-		$.ajax({
-			data: $('#id_password2, #id_password1').serialize(),
-			type: 'GET',
-			url: passwordValidationURL,
-			success: function (response) {
-				if (response.missmatch) {
-					$('#id_password1').removeClass('is-valid').addClass('is-invalid');
-					$('#id_password2').removeClass('is-valid').addClass('is-invalid');
-				} else {
-					$('#id_password1').removeClass('is-invalid').addClass('is-valid');
-					$('#id_password2').removeClass('is-invalid').addClass('is-valid');
-				}
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Send two password fields to server to validate them
+    $('#id_password2, #id_password1').keyup(function () {
+        $.ajax({
+            data: $('#id_password2, #id_password1').serialize(),
+            type: 'GET',
+            url: passwordValidationURL,
+            success: function (response) {
+                if (response.missmatch) {
+                    $('#id_password1, #id_password2').removeClass('is-valid').addClass('is-invalid');
+                } else {
+                    $('#id_password1, #id_password2').removeClass('is-invalid').addClass('is-valid');
+                }
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 })
--- svg_editor/static/svg_editor/js/main.js
+++ svg_editor/static/svg_editor/js/main.js
@@ -1,161 +1,162 @@
-let currentFileName = null;  
+/**
+ * @author Kabane-UN
+ **/
+
+let currentFileName = null;
 
 $(document).ready(function () {
 
-	$.ajaxSetup({
-		headers: { "X-CSRFToken": token }
-	});
+    // Get csrf_token
+    $.ajaxSetup({
+        headers: {"X-CSRFToken": token}
+    });
 
-	$('#saveFileButton').click(function () {
-		$.ajax({
-			data: {
-				file_name: document.getElementById('fileNameInput').value,
-				save_as: false,
-				svg: getPictureAsSvg(),
-				type: document.getElementById('save_file_type').value,
-			},
-			type: 'POST',
-			url: saveURL,
-			success: function (response) {
-				alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно сохранен!');
-				document.getElementById('fileNameInput').value = response.file_name.slice(0,-4);
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Send svg to the server to save it
+    $('#saveFileButton').click(function () {
+        $.ajax({
+            data: {
+                file_name: document.getElementById('fileNameInput').value,
+                save_as: false,
+                svg: getPictureAsSvg(),
+                type: document.getElementById('save_file_type').value,
+            },
+            type: 'POST',
+            url: saveURL,
+            success: function (response) {
+                alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно сохранен!');
+                document.getElementById('fileNameInput').value = response.file_name.slice(0, -4);
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$('#saveAsFileButton').click(function () {
-		$.ajax({
-			data: {
-				file_name: document.getElementById('fileNameInput').value,
-				save_as: true,
-				svg: getPictureAsSvg(),
-				type: document.getElementById('save_file_type').value,
-			},
-			type: 'POST',
-			url: saveURL,
-			success: function (response) {
-				alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно создан!');
-				document.getElementById('fileNameInput').value = response.file_name.slice(0,-4);
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Send svg to the server to save it as
+    $('#saveAsFileButton').click(function () {
+        $.ajax({
+            data: {
+                file_name: document.getElementById('fileNameInput').value,
+                save_as: true,
+                svg: getPictureAsSvg(),
+                type: document.getElementById('save_file_type').value,
+            },
+            type: 'POST',
+            url: saveURL,
+            success: function (response) {
+                alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно создан!');
+                document.getElementById('fileNameInput').value = response.file_name.slice(0, -4);
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$('#file').change(function () {
-		let data = new FormData();
-		data.append('file', $("#file")[0].files[0]);
-		$.ajax({
-			data: data,
-			type: 'POST',
-			url: uploadURL,
-			processData: false,
-			cache: false,
-			contentType: false,
-			success: function (response) {
-				alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно загружен!');
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			}
-		});
-		return false;
-	});
+    // Upload users file to the server
+    $('#file').change(function () {
+        let data = new FormData();
+        data.append('file', $("#file")[0].files[0]);
+        $.ajax({
+            data: data,
+            type: 'POST',
+            url: uploadURL,
+            processData: false,
+            cache: false,
+            contentType: false,
+            success: function (response) {
+                alert('Поздравляем! Файл с названием ' + response.file_name + ' успешно загружен!');
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$('#downloadButton').click(function () {
-		$.ajax({
-			url: downloadURL,
-			type: 'GET',
-			data: {
-				file_name: currentFileName
-			},
-			success: function (response) {
-				let a = document.createElement("a");
-				a.href = "/files_download?file_name=" + currentFileName;
-				a.click();
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			},
-		})
-	});
+    // Download file from server
+    $('#downloadButton').click(function () {
+        $.ajax({
+            url: downloadURL,
+            type: 'GET',
+            data: {
+                file_name: currentFileName
+            },
+            success: function () {
+                let a = document.createElement("a");
+                a.href = "/files_download?file_name=" + currentFileName;
+                a.click();
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            },
+        })
+    });
 
-	$('#target').click(function () {
-		// создаем AJAX-вызов
-		$.ajax({ // получаем данные формы
-			// тут используется шаблонизатор
-			url: viewURL,
-			// если успешно, то
-			success: function (response) {
-				document.getElementById('text').innerHTML = "List of the saved files:";
-				let list = document.querySelector('#list_svg');
-				list.innerHTML = "";
-				let key;
-				for (key in response.svgs) {
-					list.innerHTML += `<input class="inner_list_svg" type='radio' name='selected_file'>${response.svgs[key]}`
-				}
-			},
-			// если ошибка, то
-			error: function (response) {
-				// предупредим об ошибке
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors)
-			}
-		});
-		return false;
-	});
+    // Get list of user files at server
+    $('#target').click(function () {
+        $.ajax({
+            url: viewURL,
+            type: 'GET',
+            success: function (response) {
+                document.getElementById('text').innerHTML = "List of the saved files:";
+                let list = document.querySelector('#list_svg');
+                list.innerHTML = "";
+                let key;
+                for (key in response.svgs) {
+                    list.innerHTML += `<input class="inner_list_svg" type='radio' name='selected_file'>${response.svgs[key]}`
+                }
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            }
+        });
+        return false;
+    });
 
-	$("#deleteButton").click(function () {
-		$.ajax({
-			url: deleteURL,
-			type: 'POST',
-			data: {
-				file_name: currentFileName,
-				all: document.getElementById("deleteAll").checked,
-			},
-			success: function (response) {
-				alert('Поздравляем! ' + response.num_of_del + ' файл(ов) успешно удалено!');
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			},
-		})
-	})
+    // Delete users files from server
+    $("#deleteButton").click(function () {
+        $.ajax({
+            url: deleteURL,
+            type: 'POST',
+            data: {
+                file_name: currentFileName,
+                all: document.getElementById("deleteAll").checked,
+            },
+            success: function (response) {
+                alert('Поздравляем! ' + response.num_of_del + ' файл(ов) успешно удалено!');
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            },
+        })
+    })
 
-	$("#editButton").click(function () {
-		$.ajax({
-			url: loadURL,
-			type: 'GET',
-			data: {
-				file_name: currentFileName,
-			},
-			success: function (response) {
-				if (response.file_name.split('.').pop().toLowerCase() === 'svg') {
-					openAsSvg(response.svg);
-				} else {
-					console.log(response.yml);
-				}
+    // Edit file from server
+    $("#editButton").click(function () {
+        $.ajax({
+            url: loadURL,
+            type: 'GET',
+            data: {
+                file_name: currentFileName,
+            },
+            success: function (response) {
+                if (response.file_name.split('.').pop().toLowerCase() === 'svg') {
+                    openAsSvg(response.svg);
+                } else {
+                    console.log(response.yml);
+                }
+            },
+            error: function (response) {
+                alert(response.responseJSON.errors);
+            },
+        })
+    })
 
-			},
-			error: function (response) {
-				alert(response.responseJSON.errors);
-				console.log(response.responseJSON.errors);
-			},
-		})
-	})
-
-	$('#list_svg').on("click",".inner_list_svg",function () {
-		currentFileName = this.nextSibling.textContent;
-	})
+    // Choosing a current svg
+    $('#list_svg').on("click", ".inner_list_svg", function () {
+        currentFileName = this.nextSibling.textContent;
+    })
 })
