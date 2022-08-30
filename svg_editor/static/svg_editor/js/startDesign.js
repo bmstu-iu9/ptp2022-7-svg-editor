37b688c6a277d920835bebc93c3d6939e7ab0f78
Kabane-UN <andryushka.kaban.z@gmail.com>
2022-08-30 17:44:00 +0300

    Дизайн соединен с основной логикой

--- svg_editor/static/svg_editor/js/startDesign.js
+++ svg_editor/static/svg_editor/js/startDesign.js
@@ -83,6 +83,7 @@ $(document).ready(function () {
             .parents(".custom-select")
             .find(".custom-select-trigger")
             .text($(this).text());
+        $('#save_file_type').trigger('change');
     });
 
     // File input customization
